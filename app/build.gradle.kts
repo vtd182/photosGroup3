823264da97f02c5e92573e616d4355c0e4f02698
hoangkhoachau <21120481@student.hcmus.edu.vn>
2023-11-09 10:50:06 +0000

    fix sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.photosGroup3"
-        minSdk = 33
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
