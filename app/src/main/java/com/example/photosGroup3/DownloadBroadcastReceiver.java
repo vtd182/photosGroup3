70d8253c990d8cd9d06fe3a01b1e129aafc345a8
hoangkhoachau <21120481@student.hcmus.edu.vn>
2023-11-19 10:01:18 +0000

    add download broadcast receiver

---
+++ app/src/main/java/com/example/photosGroup3/DownloadBroadcastReceiver.java
@@ -0,0 +1,37 @@
+package com.example.photosGroup3;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Objects;
+
+public class DownloadBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        ImageDisplay ic = ImageDisplay.getInstance();
+        if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+            String[] result = new String[1];
+            ic.dates.add((new Date()).toString());
+            File kl = new File(ic.fullNameFile);
+            ic.size.add(Integer.parseInt(String.valueOf(kl.length() / 1024)));
+            result[0] = ic.fullNameFile;
+            if (Objects.equals(ic.fullNameFile, "")) {
+                Toast.makeText(ic.getContext(), "Download error, please try again", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Toast.makeText(ic.getContext(), "Download down", Toast.LENGTH_SHORT).show();
+            ((MainActivity) ic.requireContext()).addImageUpdate(result);
+            // ((MainActivity)getContext()).DownloadImageFromURLVer3(url_input[0].trim(),url_input[1].trim());
+            ic.notifyChangeGridLayout();
+        }
+
+    }
+}
